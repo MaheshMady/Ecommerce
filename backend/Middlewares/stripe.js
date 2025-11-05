aa6971f9feb8e1be3a1d2636d0fcbf868758a1f6
Mahesh-27 <163661354+MaheshMady@users.noreply.github.com>
2025-11-05 18:41:27 +0530

    Update stripe.js

--- backend/Middlewares/stripe.js
+++ backend/Middlewares/stripe.js
@@ -9,8 +9,8 @@ exports.createCheckoutSession = function (lineitems, orderdetail) {
       orderid: String(orderdetail._id),
       userid: String(orderdetail.userid),
     },
-    success_url: "ecommerce-alpha-teal.vercel.app/success?session_id={CHECKOUT_SESSION_ID}",
-    cancel_url: "ecommerce-alpha-teal.vercel.app/failure?session_id={CHECKOUT_SESSION_ID}",
+    success_url: "https://ecommerce-alpha-teal.vercel.app/success?session_id={CHECKOUT_SESSION_ID}",
+    cancel_url: "https://ecommerce-alpha-teal.vercel.app/failure?session_id={CHECKOUT_SESSION_ID}",
   });
 
   return session;
