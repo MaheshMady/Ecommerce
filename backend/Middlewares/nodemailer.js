28e61d7adafc43f52405fde2a96a99ce9f04792c
Mahesh-27 <163661354+MaheshMady@users.noreply.github.com>
2025-11-05 18:49:16 +0530

    Update nodemailer.js

--- backend/Middlewares/nodemailer.js
+++ backend/Middlewares/nodemailer.js
@@ -3,7 +3,9 @@ const dotenv = require("dotenv")
 require("dotenv").config()
 
 const transport = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.MAIL,
     pass: process.env.PASS,
